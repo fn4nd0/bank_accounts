42411f7df29e4be46432a9d7f8d891076e2d8a9c
Fernando Mendes <mendesfnando@gmail.com>
2023-04-11 10:59:56 -0300

    #1 - creating account process

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 // external modules
-// const chalk = require('chalk')
-// const inquirer = require('inquirer')
 import chalk from 'chalk';
 import inquirer from 'inquirer'
 
 
 // internal modules
-const fs = import('fs')
+import fs from 'fs'
 
 operation()
 
@@ -18,7 +16,7 @@ function operation() {
             name: 'action',
             message: 'What do you wanna do?',
             choices: [
-                'Create account',
+                'Create Account',
                 'Account balance',
                 'Deposit',
                 'Withdraw',
@@ -29,8 +27,46 @@ function operation() {
     .then((answer) => {
 
         const action = answer.action
-        console.log(`You chose ${action}`)
+        
+        if (action === 'Create Account')
+            createAccount()
+
     })
     .catch(err => console.error(err))
+}
 
+// Create Account
+function createAccount() {
+    console.log(chalk.bgGreen.black('Contratulations for chosing our bank!'))
+    console.log(chalk.green(`Define your account settings next`))
+    buildAccount()
+}
+
+function buildAccount() {
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Chose a name for your account:'
+        },
+    ])
+    .then((answer) => {
+        const accountName = answer.accountName
+        console.info(accountName)
+
+        if (!fs.existsSync('accounts'))
+            fs.mkdirSync('accounts')
+        
+        if (fs.existsSync(`accounts/${accountName}.json`)) {
+            console.log(chalk.bgRed.black('This account already exists, chose another name!'))
+            buildAccount()
+            return
+        }
+            
+        fs.writeFileSync(`accounts/${accountName}.json`, '{"balance": 0}', function(err) {
+            console.log(err)
+        })
+
+        operation()
+        
+    })
 }
